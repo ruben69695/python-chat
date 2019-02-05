f0f8044a64b84b3ab17bcf63d3dbbfe1da124019
david1707 <DavidAlacant@gmail.com>
2019-02-05 17:45:20 +0100

    RequestHandler documentation added

--- server/RequestHandler.py
+++ server/RequestHandler.py
@@ -2,8 +2,34 @@ import threading
 import socketserver
 
 class RequestHandler(socketserver.BaseRequestHandler):
+    """
+    Handles HTTP request made to the server, being instantiated
+    with each request. Extends BaseRequestHandler.
+
+    ...
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    handle(self):
+        Process incoming requests
+    """
 
     def handle(self):
+        """ Method that handles each request made to the server
+
+        Parameters
+        ----------
+        None
+
+        Raises
+        ------
+        None
+        """
+
         data = str(self.request.recv(1024), 'utf8')
         cur_thread = threading.current_thread()
         response = bytes("{} : {}".format(cur_thread.name, data), 'utf8')
